2bbaaec53fc5102bc4b617796b13172b314a529e
burhancoulibaly <burhancoulibaly@gmail.com>
2016-10-25 16:15:40 -0400

    got oauth to work

---
+++ backend/server.js
@@ -0,0 +1,60 @@
+var logger          = require('morgan'),
+    cors            = require('cors'),
+    http            = require('http'),
+    express         = require('express'),
+    errorhandler    = require('errorhandler'),
+    dotenv          = require('dotenv'),
+    bodyParser      = require('body-parser'),
+    request         = require('request');
+
+  var app = express();
+
+  var username = "OiJpHsTjbvJndQ";
+  var password = "OEUI53WE4fVuiK4n_XuIiFDgq00";
+  var redirect_uri = 'http://127.0.0.1:8080/frontend/app.html';
+  var auth = "Basic " + new Buffer(username + ":" + password).toString("base64");  
+  var url = "https://www.reddit.com/api/v1/access_token";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(function(err, req, res, next) {
+  if (err.name === 'StatusError') {
+    res.send(err.status, err.message);
+  } else {
+    next(err);
+  }
+  res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
+});
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger('dev'));
+  app.use(errorhandler());
+};
+
+app.post('/gettoken',(req, res)=>{
+  query_string = req.body;
+  code = query_string.code;
+   var params = 'grant_type=authorization_code&code='+code+'&redirect_uri='+redirect_uri;
+	request.post({
+    url:url,
+    body:params,
+    headers:{
+      "Authorization": auth,
+      "Content-Type":"application/x-www-form-urlencoded",
+    },
+  },(err,response,body)=>{
+    if(err){
+    res.send(err);
+    } else{
+    res.send(body);
+    }
+  });
+});
+
+var port = process.env.PORT || 3002;
+
+http.createServer(app).listen(port, function (err) {
+  console.log('listening in http://localhost:' + port);
+});
--- frontend/app.js
+++ frontend/app.js
@@ -1,14 +1,35 @@
 var  PageApp = angular.module('AppPage',['ngRoute']);
 	PageApp.controller('AppCtrl',['$scope','$http',($scope,$http)=>{
 
-		$http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
-		
+		var QueryString = function () {
+		  // This function is anonymous, is executed immediately and 
+		  // the return value is assigned to QueryString!
+		  var query_string = {};
+		  var query = window.location.search.substring(1);
+		  var vars = query.split("&");
+		  for (var i=0;i<vars.length;i++) {
+		    var pair = vars[i].split("=");
+		        // If first entry with this name
+		    if (typeof query_string[pair[0]] === "undefined") {
+		      query_string[pair[0]] = decodeURIComponent(pair[1]);
+		        // If second entry with this name
+		    } else if (typeof query_string[pair[0]] === "string") {
+		      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+		      query_string[pair[0]] = arr;
+		        // If third or later entry with this name
+		    } else {
+		      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+		    }
+		  } 
+		  return query_string;
+		}();
+
 		var req = {
 				method:'POST',
-				url:'https://www.reddit.com/api/v1/access_token',
-				data:'grant_type=authorization_code&code=CODE&redirect_uri=https://127.0.0.1:8080/frontend/app.html',
+				url:'http://localhost:3002/gettoken',
+				data: QueryString,
 				headers:{
-					'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+					'Content-Type':'application/json',
 				}
 				};
 		$http(req).then(function successCallback(response){
